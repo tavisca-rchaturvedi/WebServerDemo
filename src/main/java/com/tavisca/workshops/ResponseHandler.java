a4dd6ccefef111d1f9bf995045bf5a6e074224a8
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-07-31 15:14:19 +0530

    Webserver Implemented, Working using threads.

--- src/main/java/com/tavisca/workshops/ResponseHandler.java
+++ src/main/java/com/tavisca/workshops/ResponseHandler.java
@@ -1,4 +1,49 @@
 package com.tavisca.workshops;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Date;
+
 public class ResponseHandler {
+    public ResponseHandler(){
+
+    }
+
+    public String fileResponse(File fileToSend) throws IOException {
+        String response;
+        if(fileToSend.exists()){
+            response = "HTTP/1.1 200 OK\r\n";
+            response += "Server: My Java HTTP Server: 1.0\r\n";
+            response += "Date: " + (new Date()).toString() + "\r\n";
+            response += "Content-Type: text/html\r\n";
+            response += "Content-length: " + fileToSend.length()+"\r\n";
+            response += "\r\n";
+            response += String.join("",Files.readAllLines(fileToSend.toPath()));
+        }
+        else{
+            response = "there is no file";
+        }
+
+
+        return response;
+    }
+
+    public String method404(File fileToSend) throws IOException {
+        String response;
+        if(fileToSend.exists()){
+            response = "HTTP/1.1 404\r\n";
+            response += "Server: My Java HTTP Server: 1.0\r\n";
+            response += "Date: " + (new Date()).toString() + "\r\n";
+            response += "Content-Type: text/html\r\n";
+            response += "Content-length: " + fileToSend.length()+"\r\n";
+            response += "\r\n";
+            response += String.join("",Files.readAllLines(fileToSend.toPath()));
+        }
+        else{
+            response = "There is no file";
+        }
+
+        return response;
+    }
 }
--- src/main/java/com/tavisca/workshops/Server.java
+++ src/main/java/com/tavisca/workshops/Server.java
@@ -10,6 +10,8 @@ public class Server implements Runnable {
     int port;
     Socket clientSocket;
 
+    String baseAddress = "";
+
     public Server(int port, Socket clientSocket){
         this.port = port;
         this.clientSocket = clientSocket;
@@ -18,38 +20,42 @@ public class Server implements Runnable {
     public void run(){
 
         RequestHandler requestHandler;
+        ResponseHandler responseHandler;
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
             PrintWriter printWriter = new PrintWriter(this.clientSocket.getOutputStream(), true);
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-            String line = bufferedReader.readLine(), request = "";
+            String line = bufferedReader.readLine();
             requestHandler = new RequestHandler(line);
+            responseHandler = new ResponseHandler();
             System.out.println(Thread.currentThread().getId() + " is the thread running for " + this.clientSocket.getLocalPort() + " with request " + requestHandler.getRequestURI());
 
             if(requestHandler.getMethod().equals("GET")){
 
                 if(requestHandler.getRequestURI().trim().equals("/")){
-                    printWriter.println("<html><body><h1>Hello</h1></body></html>");
-                    bufferedWriter.write(requestHandler.getRequestURI() + " 200 OK\r\n\r\n".getBytes("UTF-8"));
+                    this.clientSocket.getOutputStream().write(responseHandler.fileResponse(new File(this.baseAddress + "OkResponse.html")).getBytes());
+                    System.out.println(responseHandler.fileResponse(new File(this.baseAddress + "OkResponse.html")));
                     System.out.println("This will send 200 response");
                 }
                 else{
                     System.out.println("The uri for this request is " + requestHandler.getRequestURI());
-                    if(new File(requestHandler.getRequestURI().substring(1)).isFile()){
+                    if(new File(this.baseAddress + requestHandler.getRequestURI().substring(1)).isFile()){
                         System.out.println("File exists with file name " + requestHandler.getRequestURI().substring(1));
+                        System.out.println(responseHandler.fileResponse(new File(this.baseAddress + "OkResponse.html")));
+                        this.clientSocket.getOutputStream().write(responseHandler.fileResponse(new File(this.baseAddress + requestHandler.getRequestURI().substring(1))).getBytes());
+
                     }
                     else{
                         System.out.println("File does not exist");
+                        System.out.println(responseHandler.method404(new File(this.baseAddress + "404.html")));
+                        this.clientSocket.getOutputStream().write(responseHandler.method404(new File(this.baseAddress + "404.html")).getBytes());
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
 
     public static void main(String[] args) throws IOException {
