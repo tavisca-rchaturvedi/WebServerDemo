a45fa75f7b2903c8a877664d3ef8312af3da10ab
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-07-31 12:48:31 +0530

    RequestHandler Implemented.

--- src/main/java/com/tavisca/workshops/Server.java
+++ src/main/java/com/tavisca/workshops/Server.java
@@ -8,47 +8,60 @@ public class Server implements Runnable {
 
     Thread t;
     int port;
+    Socket clientSocket;
 
-    public Server(int port){
+    public Server(int port, Socket clientSocket){
         this.port = port;
+        this.clientSocket = clientSocket;
     }
 
     public void run(){
+
+        RequestHandler requestHandler;
+        BufferedReader bufferedReader = null;
         try {
-            ServerSocket serverSocket = new ServerSocket(this.port);
+            bufferedReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            PrintWriter printWriter = new PrintWriter(this.clientSocket.getOutputStream(), true);
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+            String line = bufferedReader.readLine(), request = "";
+            requestHandler = new RequestHandler(line);
+            System.out.println(Thread.currentThread().getId() + " is the thread running for " + this.clientSocket.getLocalPort() + " with request " + requestHandler.getRequestURI());
 
-            while(true){
-                Socket socket = serverSocket.accept();
-                System.out.println("Server waiting at " + this.port);
+            if(requestHandler.getMethod().equals("GET")){
 
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-                String line = bufferedReader.readLine(), request = "";
-                System.out.println(line);
-                String[] query = line.split(" ");
-
-                if(query[0].equals("GET")){
-                    if(query[1].equals('/')){
-                        printWriter.println("<html><body><h1>Hello</h1></body></html>");
+                if(requestHandler.getRequestURI().trim().equals("/")){
+                    printWriter.println("<html><body><h1>Hello</h1></body></html>");
+                    bufferedWriter.write(requestHandler.getRequestURI() + " 200 OK\r\n\r\n".getBytes("UTF-8"));
+                    System.out.println("This will send 200 response");
+                }
+                else{
+                    System.out.println("The uri for this request is " + requestHandler.getRequestURI());
+                    if(new File(requestHandler.getRequestURI().substring(1)).isFile()){
+                        System.out.println("File exists with file name " + requestHandler.getRequestURI().substring(1));
                     }
                     else{
-                        if(new File(query[1].substring(1)).isFile()){
-//                            dataOutputStream.writeByte(new File);
-                        }
+                        System.out.println("File does not exist");
                     }
                 }
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+
     }
 
-    public static void main(String[] args) {
-        Thread t = new Thread(new Server(8080));
-        t.start();
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Server waiting at port 8080");
+
+        while(true){
+            Socket socket = serverSocket.accept();
+            System.out.println("Socket Accepted");
+            Thread t = new Thread(new Server(8080, socket));
+            t.start();
+        }
+
     }
 }
